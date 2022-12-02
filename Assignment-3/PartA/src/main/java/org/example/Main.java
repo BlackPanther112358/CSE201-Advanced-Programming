7f9cb03a42f549f5dcea249f308fde9cdeeb40c5
Aakarsh Jain <jainaakarsh09@gmail.com>
2022-12-02 07:50:06 +0530

    Add files via upload

--- Assignment-3/PartA/src/main/java/org/example/Main.java
+++ Assignment-3/PartA/src/main/java/org/example/Main.java
@@ -2,6 +2,7 @@ package org.example;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Random;
 
 public class Main {
@@ -86,24 +87,28 @@ class Simulate{
 
         sortedArray<Double> Arr = new sortedArray<>(length, this.array);
 
-        for(int i = 0; i < this.length; i++){
-            if(this.thread_cnt == 1){
-                sortArray<Double> t1 = new sortArray<Double>(0, this.length, Arr, i&1);
-                Thread th1 = new Thread(t1);
-                th1.start();
-                th1.join();
-                this.runTime += t1.getRunTime();
-            }else{
-                sortArray<Double> t1 = new sortArray<Double>(0, this.length/2, Arr, i&1);
-                sortArray<Double> t2 = new sortArray<Double>(this.length/2, this.length, Arr, i&1);
-                Thread th1 = new Thread(t1);
-                Thread th2 = new Thread(t2);
-                th1.start();
-                th2.start();
-                th1.join();
-                th2.join();
-                this.runTime += Math.max(t1.getRunTime(), t2.getRunTime());
-            }
+        if(this.thread_cnt == 1){
+            sortArray<Double> t1 = new sortArray<Double>(0, this.length, Arr);
+            Thread th1 = new Thread(t1);
+            th1.start();
+            th1.join();
+            this.runTime += t1.getRunTime();
+        }else{
+            sortArray<Double> t1 = new sortArray<Double>(0, this.length/2, Arr);
+            sortArray<Double> t2 = new sortArray<Double>(this.length/2, this.length, Arr);
+            Thread th1 = new Thread(t1);
+            Thread th2 = new Thread(t2);
+            th1.start();
+            th2.start();
+            th1.join();
+            th2.join();
+            this.runTime += Math.max(t1.getEndTime(), t2.getEndTime()) - Math.min(t1.getStartTime(), t2.getStartTime());
+            mergeArray<Double> t3 = new mergeArray<Double>(0, this.length/2, this.length, Arr.getArray());
+            Thread th3 = new Thread(t3);
+            th3.start();
+            th3.join();
+            this.runTime += t3.getRunTime();
+            Arr.setArray(t3.getArray());
         }
 
 //        for(Double i:Arr.getArray()){
@@ -116,8 +121,8 @@ class Simulate{
 }
 
 class sortedArray<T>{
-    private final int length;
-    private final T[] array;
+    private int length;
+    private T[] array;
 
     public sortedArray(int length, T[] array){
         this.length = length;
@@ -130,6 +135,11 @@ class sortedArray<T>{
 
     public T[] getArray(){
         return this.array;
+    }
+
+    public void setArray(T[] array){
+        this.array = array;
+        this.length = array.length;
     }
 
     public T getElement(int index){
@@ -146,26 +156,29 @@ class sortedArray<T>{
 
 class sortArray<T> implements Runnable{
 
-    private int startIdx;
+    private final int startIdx;
     private final int endIdx;
     private final sortedArray<T> array;
     private long startTime;
     private long endTime;
 
-    public sortArray(int startIdx, int endIdx, sortedArray<T> array, int parity){
+    public sortArray(int startIdx, int endIdx, sortedArray<T> array){
         this.startIdx = startIdx;
         this.endIdx = endIdx;
         this.array = array;
-        if(this.startIdx%2 != parity) this.startIdx++;
     }
 
     @Override
     public void run() {
         this.startTime = System.nanoTime();
         for(int i = this.startIdx; i < this.endIdx; i += 2){
-            if((i+1) >= this.endIdx) break;
-            if(((Comparable)this.array.getElement(i)).compareTo(this.array.getElement(i+1)) < 0){
-                this.array.swapElements(i);
+            int j = this.startIdx;
+            if((j&1) != (i&1)) j++;
+            for(; j < this.endIdx; j += 2){
+                if((j + 1) >= this.endIdx) break;
+                if(((Comparable)this.array.getElement(j)).compareTo(this.array.getElement(j+1)) < 0){
+                    this.array.swapElements(j);
+                }
             }
         }
         this.endTime = System.nanoTime();
@@ -173,6 +186,64 @@ class sortArray<T> implements Runnable{
 
     public long getRunTime(){
         return this.endTime - this.startTime;
+    }
+
+    public long getStartTime(){
+        return this.startTime;
+    }
+
+    public long getEndTime(){
+        return this.endTime;
+    }
+
+}
+
+class mergeArray<T> implements Runnable{
+
+    private final int startIdx;
+    private final int midIdx;
+    private final int endIdx;
+    private T[] array;
+    private long startTime;
+    private long endTime;
+
+    public mergeArray(int startIdx, int midIdx, int endIdx, T[] array){
+        this.startIdx = startIdx;
+        this.midIdx = midIdx;
+        this.endIdx = endIdx;
+        this.array = array;
+    }
+
+    @Override
+    public void run(){
+        this.startTime = System.nanoTime();
+
+        T[] temp_a = Arrays.copyOfRange(this.array, this.startIdx, this.midIdx);
+        T[] temp_b = Arrays.copyOfRange(this.array, this.midIdx, this.endIdx);
+        int a = this.midIdx - this.startIdx, b = this.endIdx - this.midIdx;
+
+        int i = 0, j = 0, k = this.startIdx;
+        while((i < a) || (j < b)){
+            if(i == a){
+                this.array[k++] = temp_b[j++];
+            }else if(j == b) {
+                this.array[k++] = temp_a[i++];
+            }else if(((Comparable)temp_a[i]).compareTo(temp_b[j]) > 0){
+                this.array[k++] = temp_a[i++];
+            }else{
+                this.array[k++] = temp_b[j++];
+            }
+        }
+
+        this.endTime = System.nanoTime();
+    }
+
+    public long getRunTime(){
+        return this.endTime - this.startTime;
+    }
+
+    public T[] getArray(){
+        return this.array;
     }
 
 }
